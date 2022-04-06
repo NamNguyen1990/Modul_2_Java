47accb016e01df795412c5c86fc9b268ed4da00b
NAM <namxuan.nguyen1990@gmail.com>
2022-04-06 09:53:59 +0700

    Gop_Mang

--- src/Gopmang_Java.java
+++ src/Gopmang_Java.java
@@ -2,17 +2,49 @@
 import java.util.Scanner;
 public class Gopmang_Java {
     public static void main(String[] args) {
-        int[] a = {1,2,3,4};
-        int[] b = {5,6,7};
-        int[] c = new int[a.length+ b.length];
-         for (int i=0; i< a.length; i++) {
-             c[i] = a[i];
+
+        int size1;
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Nhập số lượng phần tử mảng 1:");
+        size1 = scanner1.nextInt();
+        int[] mang1 = new int[size1];
+//      vòng lặp này sẽ duyệt i từ 0 đến chiều dài của mảng - 1
+        for (int i=0; i<size1; i++) {
+            System.out.println("Nhập vào phần tử thứ" + (i+1) + ":");
+            mang1[i] = scanner1.nextInt(); // Nhập phần tử cho mảng
+        }
+
+        System.out.println("Mảng 1 vừa nhập vào là:");
+        for (int i=0; i<size1; i++) {
+            System.out.print(mang1[i] + "\t"); // In ra mảng nhập vào
+        }
+
+        int size2;
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("\nNhập số lượng phần tử mảng 2:");
+        size2 = scanner2.nextInt();
+        int[] mang2 = new int[size2];
+//      vòng lặp này sẽ duyệt i từ 0 đến chiều dài của mảng - 1
+        for (int i=0; i<size2; i++) {
+            System.out.println("Nhập vào phần tử thứ" + (i+1) + ":");
+            mang2[i] = scanner2.nextInt(); // Nhập phần tử cho mảng
+        }
+
+        System.out.println("Mảng 2 vừa nhập vào là:");
+        for (int i=0; i<size2; i++) {
+            System.out.print(mang2[i] + "\t"); // In ra mảng nhập vào
+        }
+
+
+        int[] c = new int[mang1.length+ mang2.length];
+         for (int i=0; i< mang1.length; i++) {
+             c[i] = mang1[i];
          }
-         for (int i=0; i< b.length; i++) {
-             c[a.length+i] = b[i];
+         for (int i=0; i< mang2.length; i++) {
+             c[mang1.length+i] = mang2[i];
          }
          for (int i=0; i< c.length; i++) {
-             System.out.println(c[i]);
+             System.out.println("\t"+c[i]);
          }
     }
 }
