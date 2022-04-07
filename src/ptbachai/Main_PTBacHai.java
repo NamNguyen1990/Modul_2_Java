e9df7c06fe081956a3f82bf074d0ba7524b46dcf
NAM <namxuan.nguyen1990@gmail.com>
2022-04-07 13:02:33 +0700

    PTBacHai_Java

---
+++ src/ptbachai/Main_PTBacHai.java
@@ -0,0 +1,44 @@
+package ptbachai;
+
+import java.util.Scanner;
+
+public class Main_PTBacHai {
+    public static void main(String[] args) {
+        double a;
+        double b;
+        double c;
+        double x;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập a vào đi");
+        a = scanner.nextDouble();
+        System.out.println("Nhập b vào đi");
+        b = scanner.nextDouble();
+        System.out.println("Nhập c vào đi");
+        c = scanner.nextDouble();
+
+        PTBacHai ptBacHai = new PTBacHai(a,b,c);
+
+        if (a==0) {
+            if (b==0) {
+                System.out.println("Phương trình vô nghiệm");
+            }
+            else {
+                x = (-c/b);
+                System.out.println("Phương trình có 1 nghiệm x =" + x);
+            }
+        }
+        else {
+
+            if (ptBacHai.getDelta() < 0) {
+                System.out.println("Phương trình vô nghiệm");
+            }
+            if (ptBacHai.getDelta() == 0) {
+                System.out.println("Phương trình có nghiệm kép x1 = x2 = " + ptBacHai.getno1());
+            }
+            if (ptBacHai.getDelta() > 0) {
+                System.out.println("Phương trình có nghiệm thứ nhất là x1 = " + ptBacHai.getno1());
+                System.out.println("Phương trình có nghiệm thứ hai là x2 = " + ptBacHai.getno2());
+            }
+        }
+    }
+}
---
+++ src/ptbachai/PTBacHai.java
@@ -0,0 +1,58 @@
+package ptbachai;
+
+public class PTBacHai {
+    double a;
+    double b;
+    double c;
+
+    public PTBacHai() {
+    }
+
+    public PTBacHai(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public double getDelta() {
+        return (b * b - 4 * a * c);
+    }
+
+
+    public double getno1() {
+        double delta = getDelta();
+        double no1 = (-b + Math.sqrt(delta)/(2*a));
+        return no1;
+    }
+
+    public double getno2() {
+        double delta = getDelta();
+        double no2 = (-b - Math.sqrt(delta)/(2*a));
+        return no2;
+    }
+}
